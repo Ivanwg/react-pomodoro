af184930b5499c0d3b561a87bcca67b0fce67259
Ivan Kolontaevsky <guiteache@gmail.com>
2024-12-21 18:07:02 +0300

    skelet task store added

--- src/features/add-task-form.tsx
+++ src/features/add-task-form.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { cn } from '@/shared/lib/utils'
 import { Button, Input } from '@/shared/ui'
+import { useTasksSelector } from '@/entities/task/store/selector'
 
 interface AddTaskFormProps {
   className?: string
 }
 
 export const AddTaskForm: React.FC<AddTaskFormProps> = ({ className }) => {
+  const [value, setValue] = useState('')
+  const { loading } = useTasksSelector((state) => state)
+
+  const onAddTask = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // AddTask(value)  В этом модуле предусмотреть дефолтные пропсы
+  }
   return (
-    <form className={cn('flex gap-3', className)}>
-      <Input placeholder=' Название задачи' />
-      <Button>
+    <form className={cn('flex gap-3', className)} onSubmit={onAddTask}>
+      <Input placeholder='Название задачи' value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button loading={loading}>
         Добавить
       </Button>
     </form>
